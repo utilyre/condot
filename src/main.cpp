b2f1d80ab2a4ae1b1eec6efeca1458aca96a5573
Amirabbas Fazelinia <utilyre@gmail.com>
2024-05-24 11:48:01 +0330

    feat(main): take initial input from user

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,54 @@
+#include <cstdlib>
 #include <iostream>
-using namespace std;
+#include <string>
+#include <vector>
+
+struct Player {
+  Player(std::string name, int age, std::string color)
+      : name(name), age(age), color(color) {}
+
+  std::string name;
+  int age;
+  std::string color;
+};
 
 int main() {
-  cout << "hello world\n";
+  std::cout << "Welcome to Condottiere\n\n";
+
+  int numPlayers;
+  std::cout << "How many players are you? ";
+  std::cin >> numPlayers;
+  if (numPlayers < 3) {
+    std::cout << "You need to be at least 3 to play this game\n";
+    std::exit(EXIT_FAILURE);
+  }
+  if (numPlayers > 6) {
+    std::cout << "You can be at most 6 to play this game\n";
+    std::exit(EXIT_FAILURE);
+  }
+
+  std::vector<Player> players;
+  for (int i = 0; i < numPlayers; i++) {
+    std::cout << "\nPlayer #" << i + 1 << ":\n";
+
+    std::string name;
+    std::cout << "Name: ";
+    std::cin >> name;
+
+    int age;
+    std::cout << "Age: ";
+    std::cin >> age;
+
+    std::string color;
+    std::cout << "Color: ";
+    std::cin >> color;
+
+    players.emplace_back(name, age, color);
+  }
+
+  for (auto p : players) {
+    std::cout << "-----------------\n";
+    std::cout << p.name << " in the age of " << p.age << " has color " << p.color
+              << '\n';
+  }
 }
