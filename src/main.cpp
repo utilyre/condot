bd2dfc81b4addced3285755b052bc9e59792ddf1
hidenvar <s.amirm.fazeli@gmail.com>
2024-05-27 12:18:16 +0330

    feat(main): shuffling functonality , dealing cards to players

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <algorithm>
+#include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <random>
 #include <string>
 #include <vector>
 
@@ -8,9 +10,11 @@ struct Player {
   Player(std::string name, int age, std::string color)
       : name(name), age(age), color(color) {}
 
-  std::string name;
-  int age;
-  std::string color;
+  std::string              name;
+  int                      age;
+  std::string              color;
+  std::vector<std::string> cards_available;
+  std::vector<std::string> cards_played;
 };
 
 int main() {
@@ -46,13 +50,37 @@ int main() {
 
     players.emplace_back(name, age, color);
   }
+  std::random_device rd;
+  std::mt19937 g(rd());
   std::vector<std::string> cards{"1","1","1","1","1","1","1","1","1","1","2","2","2","2","2","2","2","2","3","3","3","3","3","3","3","3","4","4","4","4","4","4","4","4",
                                  "5","5","5","5","5","5","5","5","6","6","6","6","6","6","6","6","10","10","10","10","10","10","10","10","matarsak","matarsak","matarsak",
                                  "matarsak","matarsak","matarsak","matarsak","matarsak","matarsak","matarsak","matarsak","matarsak","matarsak","matarsak","matarsak","matarsak",
                                  "tablzan","tablzan","tablzan","tablzan","tablzan","tablzan","bahar","bahar","bahar","zemestan","zemestan","zemestan","shirdokht","shirdokht","shirdokht"};
-  for (auto p : players) {
-    std::cout << "-----------------\n";
-    std::cout << p.name << " in the age of " << p.age << " has color " << p.color
-              << '\n';
+ 
+   std::vector<std::string> playingCards{cards}; 
+   shuffle(cards.begin(),cards.end(),g);
+   
+   for(int i{}; i < numPlayers;i++){
+     for(int j{}; j < 10 ; j++){  
+      players[i].cards_available.push_back(cards.back());
+      cards.pop_back();
+     }
+   }
+   
+    getchar();
+    
+    for (auto p : players) {
+      system("clear");
+      std::cout << "-----------------\n";
+      std::cout << p.name << " in the age of " << p.age << " has color " << p.color
+                << "\npress enter to show cards!";
+      getchar();
+      system("clear");
+      for(auto c : p.cards_available){
+        std::cout <<  c << "  "; 
+      }
+    
+      std::cout << "\n\npress any key to continue" ;
+      getchar();    
   }
 }
