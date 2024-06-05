fe34dd195f6036d99da16504dbee834d241e6032
hidenvar <s.amirm.fazeli@gmail.com>
2024-06-05 20:53:36 +0330

    new classes added

---
+++ include/normalcards.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <cards.hpp>
+class NormalCards: public Cards{
+  public:
+    
+  private:
+    int power{};
+};
---
+++ include/specialcards.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <cards.hpp>
+class SpecialCards: public Cards{
+  public:
+
+  private:
+    
+};
