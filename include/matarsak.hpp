cbe4ade2a7a78c9cf73d99624d07a30b55ed0aea
hidenvar <s.amirm.fazeli@gmail.com>
2024-06-25 20:30:02 +0330

    feat(matarsak): proper methods added

---
+++ include/matarsak.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <specialcard.hpp>
+
+class Matarsak : public SpecialCard{
+  public:
+    Matarsak(std::string name);
+    void Ability();
+  private:
+};
---
+++ src/matarsak.cpp
@@ -0,0 +1,8 @@
+#include <specialcard.hpp>
+#include <matarsak.hpp>
+
+Matarsak::Matarsak(std::string name) : SpecialCard(name) {}
+
+void Matarsak::Ability(){
+  
+}
