8aaea306c5d59fe983bc6493146642bc3c4f46c4
Amirabbas Fazelinia <utilyre@gmail.com>
2024-07-04 15:24:54 +0330

    feat(asset_manager): load all assets

--- include/asset_manager.hpp
+++ include/asset_manager.hpp
@@ -8,4 +8,13 @@ struct AssetManager
   ~AssetManager();
 
   Texture2D Map;
+
+  Texture2D Bishop;
+  Texture2D Drummer;
+  Texture2D Heroine;
+  Texture2D Scarecrow;
+  Texture2D Spring;
+  Texture2D Spy;
+  Texture2D Turncoat;
+  Texture2D Winter;
 };
--- src/asset_manager.cpp
+++ src/asset_manager.cpp
@@ -5,9 +5,29 @@
 void AssetManager::Load()
 {
   Map = LoadTexture("assets/map.png");
+
+  Bishop = LoadTexture("assets/bishop.png");
+  Drummer = LoadTexture("assets/drummer.png");
+  Heroine = LoadTexture("assets/heroine.png");
+  Scarecrow = LoadTexture("assets/scarecrow.png");
+  Spring = LoadTexture("assets/spring.png");
+  Spy = LoadTexture("assets/spy.png");
+  Turncoat = LoadTexture("assets/turncoat.png");
+  Winter = LoadTexture("assets/winter.png");
 }
 
 AssetManager::~AssetManager()
 {
+  // NOTE: reverse ordering recommended
+
+  UnloadTexture(Winter);
+  UnloadTexture(Turncoat);
+  UnloadTexture(Spy);
+  UnloadTexture(Spring);
+  UnloadTexture(Scarecrow);
+  UnloadTexture(Heroine);
+  UnloadTexture(Drummer);
+  UnloadTexture(Bishop);
+
   UnloadTexture(Map);
 }
