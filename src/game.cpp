0793565ad1c647dbf118180c4d457be2c22786a7
Amirabbas Fazelinia <utilyre@gmail.com>
2024-07-04 15:39:02 +0330

    feat(game): draw a place for each player's cards

--- src/game.cpp
+++ src/game.cpp
@@ -44,5 +44,12 @@ void Game::Render() const
 
   ClearBackground(RAYWHITE);
 
+  int length = 3 * height / 4;
+  int thickness = 200;
+  DrawRectangle((width - length) / 2, 0, length, thickness, RED); // top
+  DrawRectangle(width - thickness, (height - length) / 2, thickness, length, GREEN); // right
+  DrawRectangle((width - length) / 2, height - thickness, length, thickness, BLUE); // bottom
+  DrawRectangle(0, (height - length) / 2, thickness, length, GRAY); // left
+
   DrawTexture(m_Assets.Map, (width - m_Assets.Map.width) / 2, (height - m_Assets.Map.height) / 2, WHITE);
 }
