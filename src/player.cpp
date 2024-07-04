d473e7ea3d9e46d971037489956c0959612b19de
hidenvar <s.amirm.fazeli@gmail.com>
2024-07-05 00:37:59 +0330

    feat(player): showing the cards to all players

--- src/player.cpp
+++ src/player.cpp
@@ -33,32 +33,56 @@ void Player::Render(const AssetManager& assets) const
 
   int length = 3 * height / 4;
   int thickness = 200;
-  float ratio = 0.75;
-  Vector2 bottom{(width - height) / 2 + 150, (height - assets.Drummer.height) / 2 + 470};
   
+  Vector2 bottom{(width - height) / 2 + 150, (height - assets.Drummer.height) / 2 + 470};
+  Vector2 left{200, (height - length) / 2};
+  Vector2 top{(width - length) / 2 + 140, 200};
+  Vector2 right{width - thickness, (height - length) / 2 + 120};
+
   switch (m_Position)
   {
   case Position::TOP:
     DrawRectangle((width - length) / 2, 0, length, thickness, RED);
+    RenderCards(assets, top ,180);
     break;
   case Position::RIGHT:
     DrawRectangle(width - thickness, (height - length) / 2, thickness, length, GREEN);
+    RenderCards(assets, right, 270);
     break;
   case Position::BOTTOM:
     DrawRectangle((width - length) / 2, height - thickness, length, thickness, BLUE);
+    RenderCards(assets, bottom, 0);
     break;
   case Position::LEFT:
     DrawRectangle(0, (height - length) / 2, thickness, length, GRAY);
+    RenderCards(assets, left, 90);
     break;
   }
+
+}
+
+void Player::RenderCards(const AssetManager& assets, Vector2 cordinate, float rotation) const{
+float ratio = 0.75;
+if(m_Position == Position::TOP || m_Position == Position::BOTTOM)
   for(const auto& c : m_Cards){
     if(c.m_Name == "drummer"){
-        DrawTextureEx(assets.Drummer, bottom , 0.0 , ratio , WHITE);
-        bottom.x += 50;
+        DrawTextureEx(assets.Drummer, cordinate , rotation , ratio , WHITE);
+        cordinate.x += 50;
     }
     else if(c.m_Name == "bishop"){
-        DrawTextureEx(assets.Bishop, Vector2(bottom.x , bottom.y) , 0.0 , ratio , WHITE);
-        bottom.x += 50;
+        DrawTextureEx(assets.Bishop, cordinate , rotation , ratio , WHITE);
+        cordinate.x += 50;
     }
   }
+if(m_Position == Position::LEFT || m_Position == Position::RIGHT)
+  for(const auto& c : m_Cards){
+    if(c.m_Name == "drummer"){
+        DrawTextureEx(assets.Drummer, cordinate , rotation , ratio , WHITE);
+        cordinate.y += 50;
+    }
+    else if(c.m_Name == "bishop"){
+        DrawTextureEx(assets.Bishop, cordinate , rotation , ratio , WHITE);
+        cordinate.y += 50;
+    }
+  }  
 }
