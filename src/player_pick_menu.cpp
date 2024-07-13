11132ed938ca14b4109c4279516cd0b4117e174f
Amirabbas Fazelinia <utilyre@gmail.com>
2024-07-13 21:07:22 +0330

    feat(menu): take player count in player pick menu

--- src/player_pick_menu.cpp
+++ src/player_pick_menu.cpp
@@ -8,7 +8,8 @@ static const int MENU_WIDTH = 1600;
 static const int MENU_HEIGHT = 900;
 
 PlayerPickMenu::PlayerPickMenu(State* state)
-: m_State(state)
+: m_State(state),
+  m_PlayerCount(3)
 {
 }
 
@@ -17,6 +18,15 @@ void PlayerPickMenu::Update()
   if (m_State->Get() != State::PLAYER_PICK_MENU)
   {
     return;
+  }
+
+  if (IsKeyPressed(KEY_UP))
+  {
+    m_PlayerCount++;
+  }
+  if (IsKeyPressed(KEY_DOWN))
+  {
+    m_PlayerCount--;
   }
 }
 
@@ -30,6 +40,7 @@ void PlayerPickMenu::Render(const AssetManager& assets) const
   int width = GetScreenWidth();
   int height = GetScreenHeight();
 
+  int padding = 50;
   Rectangle menuDimensions = {
     (width - MENU_WIDTH) / 2.0f,
     (height - MENU_HEIGHT) / 2.0f,
@@ -38,4 +49,12 @@ void PlayerPickMenu::Render(const AssetManager& assets) const
   };
 
   DrawRectangleRounded(menuDimensions, 0.1f, 0, WHITE);
+
+  DrawText(
+    TextFormat("Players: %d (use <up> and <down> keys to adjust)", m_PlayerCount),
+    menuDimensions.x + padding,
+    menuDimensions.y + padding,
+    25,
+    BLACK
+  );
 }
