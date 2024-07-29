2a91e9a339865cf4932a77fb4b747d650dd824d3
Amirabbas Fazelinia <utilyre@gmail.com>
2024-07-29 12:09:21 +0330

    feat(file_stream): implement Stream{Writer,Reader} for class FileStream

---
+++ src/file_stream.cpp
@@ -0,0 +1,23 @@
+#include <cstdint>
+#include <filesystem>
+#include <fstream>
+
+#include <file_stream.hpp>
+
+FileStream::FileStream(const std::filesystem::path& path, std::ios::openmode mode)
+: m_Path(path)
+{
+  m_File = std::fstream(path, mode);
+}
+
+bool FileStream::WriteData(const char* data, uint64_t size)
+{
+  m_File.write(data, size);
+  return m_File.good();
+}
+
+bool FileStream::ReadData(char* data, uint64_t size)
+{
+  m_File.read(data, size);
+  return m_File.good();
+}
